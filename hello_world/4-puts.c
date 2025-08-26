71d592134deeb96c274c69925c5a6aaf297d9cb5
OWNALGARNI <s.xooxs@hotmail.com>
2025-08-27 00:54:48 +0300

    Update 4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+
 /**
-* main - Entry point
-* Return: Always 0
-*/
+ * main - نقطة البداية للبرنامج
+ *
+ * Return: دائماً 0 (نجاح)
+ */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle");
-return (0);
+    puts("Programming is like building a multilingual puzzle");
+    return (0);
 }
