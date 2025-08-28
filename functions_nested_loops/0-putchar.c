4702f70b3ffbefd87b959e33b435cb9ce873642f
OWNALGARNI <s.xooxs@hotmail.com>
2025-08-28 08:10:08 +0000

    0-putchar.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,15 +1,25 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
  * main - Entry point
  *
- * Description: Prints "_putchar" followed by a new line.
+ * Description: Prints "_putchar" followed by a new line
+ * using only the _putchar function.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-puts("_putchar");
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+
 return (0);
 }
+
