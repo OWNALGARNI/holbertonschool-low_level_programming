a7543cd237431cb67af43824931c0a81c032e364
OWNALGARNI <s.xooxs@hotmail.com>
2025-08-28 08:35:19 +0000

    0-putchar.c

--- functions_nested_loops/main.c
+++ functions_nested_loops/main.c
@@ -1,7 +1,20 @@
-#ifndef MAIN_H
-#define MAIN_H
+#include "main.h"
 
-int _putchar(char c);
-
-#endif /* MAIN_H */
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
