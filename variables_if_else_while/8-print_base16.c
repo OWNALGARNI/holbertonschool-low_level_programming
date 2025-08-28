313f51cb88ad34fb30dfc1c02dc8a3fba3507b9a
OWNALGARNI <s.xooxs@hotmail.com>
2025-08-28 07:17:52 +0000

    8-print_base16.c

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all hexadecimal digits in lowercase,
+ * followed by a new line. Uses only putchar three times.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int i;
+
+for (i = 0; i < 16; i++)
+putchar(i < 10 ? i + '0' : i - 10 + 'a');
+
+putchar('\n');
+
+return (0);
+}
