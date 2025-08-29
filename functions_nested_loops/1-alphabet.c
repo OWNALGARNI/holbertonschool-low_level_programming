6bc2ad7177059f638adcd98a4fe417b9e1b3c4ae
OWNALGARNI <s.xooxs@hotmail.com>
2025-08-29 11:33:56 +0000

    1-alphabet.c

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -6,8 +6,7 @@
 void print_alphabet(void)
 {
 char alphabet[] = "abcdefghijklmnopqrstuvwxyz\n";
-_putchar(alphabet[0]);  // First call to _putchar
-
+_putchar(alphabet[0]);
 for (int i = 1; alphabet[i] != '\0'; i++)
-_putchar(alphabet[i]);  // Second call to _putchar
+_putchar(alphabet[i]);
 }
