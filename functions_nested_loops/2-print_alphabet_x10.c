53471c8a55f1378b46c0bd0ff9d49495642cf2bc
OWNALGARNI <s.xooxs@hotmail.com>
2025-08-30 09:59:08 +0000

    5-sign.c

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,22 +1,15 @@
-#include "main.h"
+#include <unistd.h>
+
 /**
  * print_alphabet_x10 - Prints the lowercase alphabet 10 times
- *
- * This function prints the alphabet in lowercase followed by a newline,
- * repeated 10 times using _putchar.
  */
 void print_alphabet_x10(void)
 {
-char line[] = "abcdefghijklmnopqrstuvwxyz\n";
-int i;
+    char line[] = "abcdefghijklmnopqrstuvwxyz\n";
+    int i;
 
-for (i = 0; i < 10; i++)
-{
-int j = 0;
-while (line[j])
-{
-_putchar(line[j]);
-j++;
-}
-}
+    for (i = 0; i < 10; i++)
+    {
+        write(1, line, sizeof(line) - 1);
+    }
 }
--- functions_nested_loops/main.c
+++ functions_nested_loops/main.c
@@ -1,7 +1,17 @@
-void print_alphabet_x10(void);
+#include <unistd.h>
+#include "main.h"
 
 int main(void)
 {
-    print_alphabet_x10();
+    print_sign(98);
+    write(1, "\n", 1);
+
+    print_sign(0);
+    write(1, "\n", 1);
+
+    print_sign(-1);
+    write(1, "\n", 1);
+
     return 0;
 }
+
