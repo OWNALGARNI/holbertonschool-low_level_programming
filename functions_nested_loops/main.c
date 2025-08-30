27f7c741320814421539b9a95a186abe91d8dca9
root <root@ip-10-42-241-175.ec2.internal>
2025-08-30 12:23:41 +0000

    10-add.c

--- functions_nested_loops/main.c
+++ functions_nested_loops/main.c
@@ -3,10 +3,15 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 0
  */
 int main(void)
 {
-    times_table();
+    int result;
+
+    result = add(3, 7);
+    _putchar(result + '0'); /* Only works for single-digit results */
+    _putchar('\n');
+
     return (0);
 }
