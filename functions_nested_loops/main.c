653b3b15dc9e5fd97c6b19274e23cca23bd09f34
root <root@ip-10-42-241-175.ec2.internal>
2025-08-30 12:40:27 +0000

    3-islower

--- functions_nested_loops/main.c
+++ functions_nested_loops/main.c
@@ -7,9 +7,15 @@
  */
 int main(void)
 {
-    print_to_98(0);
-    print_to_98(111);
-    print_to_98(98);
-    print_to_98(-10);
+    int r;
+
+    r = _islower('a');
+    _putchar(r + '0');
+    _putchar('\n');
+
+    r = _islower('A');
+    _putchar(r + '0');
+    _putchar('\n');
+
     return (0);
 }
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -2,6 +2,6 @@
 #define MAIN_H
 
 int _putchar(char c);
-void print_to_98(int n);
+int _islower(int c);
 
 #endif
