54702373088729f853064dfcc5bca39f58b589bc
OWNALGARNI <s.xooxs@hotmail.com>
2025-09-05 12:07:29 +0000

    5-rev_string.c

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverses a string in place
+ * @s: Pointer to the string to be reversed
+ */
+void rev_string(char *s)
+{
+int i = 0, j = 0;
+char temp;
+
+while (s[j] != '\0')
+j++;
+
+j--;
+
+while (i < j)
+{
+temp = s[i];
+s[i] = s[j];
+s[j] = temp;
+i++;
+j--;
+}
+}
--- pointers_arrays_strings/main.c
+++ pointers_arrays_strings/main.c
@@ -1,9 +1,14 @@
 #include "main.h"
+#include <stdio.h>
 
 int main(void)
 {
-    char *msg = "Riyadh";
-    print_rev(msg);
+    char str[] = "Riyadh";
+
+    printf("Before: %s\n", str);
+    rev_string(str);
+    printf("After: %s\n", str);
+
     return (0);
 }
 
