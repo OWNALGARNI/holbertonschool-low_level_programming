6cc4c56b82807e79ff3897ff198ff74b2962eb03
OWNALGARNI <s.xooxs@hotmail.com>
2025-09-05 07:29:27 +0000

    5-more_numbers.c

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -6,23 +6,14 @@
 void more_numbers(void)
 {
 int line, num;
-char tens, units;
 
 for (line = 0; line < 10; line++)
 {
 for (num = 0; num <= 14; num++)
 {
-if (num > 9)
-{
-tens = '1';
-units = num % 10 + '0';
-_putchar(tens);
-_putchar(units);
-}
-else
-{
-_putchar(num + '0');
-}
+if (num >= 10)
+_putchar('1');
+_putchar((num % 10) + '0');
 }
 _putchar('\n');
 }
