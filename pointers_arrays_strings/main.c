707fc9be0b6f91be23a4dc6fc0de807bb70a2066
root <root@ip-10-42-3-158.ec2.internal>
2025-09-06 10:41:26 +0000

    0-strcat.c

--- pointers_arrays_strings/main.c
+++ pointers_arrays_strings/main.c
@@ -3,13 +3,11 @@
 
 int main(void)
 {
-    char *s1 = "+++---++123abc";
-    char *s2 = "no digits";
-    char *s3 = "--+--42";
+    char dest[100] = "Hello, ";
+    char src[] = "Riyadh!";
 
-    printf("s1: %d\n", _atoi(s1)); // Output: -123
-    printf("s2: %d\n", _atoi(s2)); // Output: 0
-    printf("s3: %d\n", _atoi(s3)); // Output: -42
+    _strcat(dest, src);
+    printf("%s\n", dest); 
 
     return (0);
 }
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -2,7 +2,7 @@
 #define MAIN_H
 
 int _putchar(char c);
-int _atoi(char *s);
+char *_strcat(char *dest, char *src);
 
 #endif /* MAIN_H */
 
