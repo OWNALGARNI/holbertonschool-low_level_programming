04068c4f47140e10f0fed027cd9f6954e11c1e45
root <root@ip-10-42-191-201.ec2.internal>
2025-09-12 20:39:54 +0000

    3-strspn.c

--- pointers_arrays_strings/main.c
+++ pointers_arrays_strings/main.c
@@ -1,27 +1,14 @@
 #include "main.h"
 #include <stdio.h>
 
-/**
- * main - Entry point to test _strchr function
- *
- * Return: Always 0 (Success)
- */
 int main(void)
 {
-    char str[] = "Hello, world!";
-    char *result;
+    char str[] = "abcde123";
+    char accept[] = "abcde";
+    unsigned int len;
 
-    result = _strchr(str, 'w');
-
-    if (result != NULL)
-    {
-        printf("Character found: %c\n", *result);
-        printf("Pointer position: %ld\n", result - str);
-    }
-    else
-    {
-        printf("Character not found.\n");
-    }
+    len = _strspn(str, accept);
+    printf("Length of prefix substring: %u\n", len);
 
     return (0);
 }
