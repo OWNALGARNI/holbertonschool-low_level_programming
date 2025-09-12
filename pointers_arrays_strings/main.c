464bba24c3c2b1bdd53553881ffd2bbd9d4eb094
root <root@ip-10-42-191-201.ec2.internal>
2025-09-12 20:30:57 +0000

    2-strchr.c

--- pointers_arrays_strings/main.c
+++ pointers_arrays_strings/main.c
@@ -1,25 +1,27 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * main - Entry point to test _memcpy function
+ * main - Entry point to test _strchr function
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    char source[] = "Hello";
-    char destination[6];
-    int i;
+    char str[] = "Hello, world!";
+    char *result;
 
-    _memcpy(destination, source, 5);
+    result = _strchr(str, 'w');
 
-    destination[5] = '\0';
-
-    for (i = 0; destination[i] != '\0'; i++)
+    if (result != NULL)
     {
-        _putchar(destination[i]);
+        printf("Character found: %c\n", *result);
+        printf("Pointer position: %ld\n", result - str);
     }
-    _putchar('\n');
+    else
+    {
+        printf("Character not found.\n");
+    }
 
     return (0);
 }
