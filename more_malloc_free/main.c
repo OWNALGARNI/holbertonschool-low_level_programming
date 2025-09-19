3683930fdee9a81f32bcd11307631bcec7725f1c
OWNALGARNI <s.xooxs@hotmail.com>
2025-09-19 23:26:07 +0000

    1-string_nconcat.c

--- more_malloc_free/main.c
+++ more_malloc_free/main.c
@@ -2,25 +2,37 @@
 #include <stdlib.h>
 
 /**
- * main - Entry point to test malloc_checked function
+ * main - Entry point to test string_nconcat function
  *
  * Return: Always 0
  */
 int main(void)
 {
-char *buffer;
-unsigned int size = 20;
+char *s1 = "Hello, ";
+char *s2 = "World!";
+char *result;
+unsigned int n = 3;
 unsigned int i;
 
-buffer = malloc_checked(size);
-for (i = 0; i < size; i++)
+result = string_nconcat(s1, s2, n);
+if (result == NULL)
 {
-buffer[i] = 'A';
-_putchar(buffer[i]);
+_putchar('E');
+_putchar('R');
+_putchar('R');
+_putchar('O');
+_putchar('R');
+_putchar('\n');
+return (1);
+}
+
+for (i = 0; result[i] != '\0'; i++)
+{
+_putchar(result[i]);
 }
 _putchar('\n');
 
-free(buffer);
+free(result);
 return (0);
 }
 
