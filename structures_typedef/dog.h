1278c4f06b635fced09e122d181482051b60bff7
OWNALGARNI <s.xooxs@hotmail.com>
2025-09-20 11:36:04 +0000

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -9,12 +9,16 @@
  */
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+char *name;
+float age;
+char *owner;
 };
 
-/* Function prototypes */
+/**
+ * dog_t - Typedef for struct dog
+ */
+typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
--- structures_typedef/main.c
+++ structures_typedef/main.c
@@ -2,19 +2,17 @@
 #include "dog.h"
 
 /**
- * main - Tests the init_dog and print_dog functions
+ * main - Demonstrates usage of dog_t typedef
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    struct dog my_dog;
+dog_t my_dog;
 
-    init_dog(&my_dog, "Ghost", 4.75, "Jon Snow");
-    print_dog(&my_dog);
+init_dog(&my_dog, "Ghost", 4.75, "Jon Snow");
+print_dog(&my_dog);
 
-    init_dog(NULL, "Ghost", 4.75, "Jon Snow");
-    print_dog(NULL);
-
-    return (0);
+return (0);
 }
+
