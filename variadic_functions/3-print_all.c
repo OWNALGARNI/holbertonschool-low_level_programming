8f7bae67a58677c0d69c0606f0a3995195726409
OWNALGARNI <s.xooxs@hotmail.com>
2025-09-27 14:42:43 +0000

    3-print_all.c

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -13,28 +13,31 @@ unsigned int i = 0;
 char *str;
 char *sep = "";
 char type;
-int is_valid;
 
 va_start(args, format);
 
 while (format && format[i])
 {
 type = format[i];
-is_valid = (type == 'c' || type == 'i' || type == 'f' || type == 's');
 
-if (is_valid)
+if (type == 'c' || type == 'i' || type == 'f' || type == 's')
 {
 printf("%s", sep);
-if (type == 'c')
-printf("%c", va_arg(args, int));
-if (type == 'i')
-printf("%d", va_arg(args, int));
-if (type == 'f')
-printf("%f", va_arg(args, double));
-if (type == 's')
+switch (type)
 {
+case 'c':
+printf("%c", va_arg(args, int));
+break;
+case 'i':
+printf("%d", va_arg(args, int));
+break;
+case 'f':
+printf("%f", va_arg(args, double));
+break;
+case 's':
 str = va_arg(args, char *);
 printf("%s", str ? str : "(nil)");
+break;
 }
 sep = ", ";
 }
