09f9af3d10a3d64ff1503f26861577889e024d72
OWNALGARNI <s.xooxs@hotmail.com>
2025-10-14 21:12:41 +0000

    fix:syntax

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,24 +1,23 @@
 # include <stdio.h>
 # include "lists.h"
 
-**/ print_list print all elements of a list _t list
-*h: pointer to the first node@
-*Rutern number of nodes
+/**
+* print_list print all elements of a list _t list
+*@ h: pointer to the first node
+* Return  number of nodes
 */
 
 size_t print_list(const list_t *h)
 {
 size_t count =0;
-while(h !=Null)
-}
-if(h->str ==Null)
+while(h !=NULL)
 {
-printf("[0](nil")\n);
-}else
-{printf("[%u]%s\n",h->len,h->str);
+if (h->str ==NULL)
+printf("[0] (nil)\n");
+else
+printf("[%u] %s\n",h->len,h->str);
 h = h->next;
 count++;
 }
-
 return count ;
 }
