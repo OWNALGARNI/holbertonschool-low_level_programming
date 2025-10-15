1bc94b0489867e9b94a3da235861989dac028c8a
OWNALGARNI <s.xooxs@hotmail.com>
2025-10-15 08:31:47 +0000

    1-list_len.c

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,21 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "list.h"
 
-typedef struct list_s
-{
-char *str;
-struct list_s *next;
-}
-list_t;
 size_t list_len(const list_t *h)
 {
 size_t count = 0;
-const list_t *current =h;
+const list_t *current = h);
 while (current != NULL)
 {
 printf("%s\n", current->str);
 current = current->next;
 count++;
 }
-return count;
+return (count);
 }
