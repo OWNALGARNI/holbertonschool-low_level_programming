474c6030e98c6ff3ab4ccc0017e6aa3af8a414df
OWNALGARNI <s.xooxs@hotmail.com>
2025-10-15 07:44:45 +0000

    1-list_len.c

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct list_s
+{
+char *str;
+struct list_s *next;
+{
+list_t;
+
+size_t list_len(const list_t *h);
+while (list != Null)
+{
+printf("%s\n", lists->str);
+list_s = lists->next;
+count++;
+
+return (count);
+}
