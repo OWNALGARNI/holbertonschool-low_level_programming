119c96ee5a40f964667b4c4d83f9d0a0fcea997a
OWNALGARNI <s.xooxs@hotmail.com>
2025-10-17 11:30:34 +0000

    fix:syntax

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,10 +1,26 @@
-#include <string.h>
 #include "lists.h"
+#include <string.h>
 
 /**
- * add_node - Adds a new node at the beginning of a linked list
- * @head: Pointer to the head of the list
- * @str: String to be duplicated and added to the new node
+ * _strlen - Calculates the length of a string
+ * @s: The string to measure
+ *
+ * Return: Length of the string
+ */
+unsigned int _strlen(const char *s)
+{
+unsigned int len = 0;
+
+while (s && s[len] != '\0')
+len++;
+
+return (len);
+}
+
+/**
+ * add_node - Adds a new node at the beginning of a list_t list
+ * @head: Double pointer to the head of the list
+ * @str: String to duplicate into the new node
  *
  * Return: Address of the new element, or NULL if it failed
  */
@@ -17,11 +33,14 @@ if (new_node == NULL)
 return (NULL);
 
 new_node->str = strdup(str);
+if (new_node->str == NULL)
+{
+free(new_node);
+return (NULL);
+}
 
-new_node->len = strlen(new_node->str);
-
+new_node->len = _strlen(new_node->str);
 new_node->next = *head;
-
 *head = new_node;
 
 return (new_node);
