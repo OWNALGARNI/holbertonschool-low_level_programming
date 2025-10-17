f8b1f753cada27d32c27c29d4e4c0aca50e03b48
OWNALGARNI <s.xooxs@hotmail.com>
2025-10-17 11:35:06 +0000

    fix:syntax

--- singly_linked_lists/lists.h
+++ singly_linked_lists/lists.h
@@ -2,24 +2,23 @@
 #define LISTS_H
 
 #include <stdlib.h>
-#include <stddef.h> 
 
 /**
- * struct list_s - singly linked list node structure
- * @str: string - malloc'ed string
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
  * @len: length of the string
  * @next: points to the next node
  *
- * Description: node structure for singly linked list used in this project
+ * Description: singly linked list node structure
  */
 typedef struct list_s
 {
-	char *str;
-	unsigned int len;
-	struct list_s *next;
+    char *str;
+    unsigned int len;
+    struct list_s *next;
 } list_t;
 
-size_t list_len(const list_t *h); 
+size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 
 #endif /* LISTS_H */
