6b517006c1fef4df5293c79b7c9d83600530f9e9
OWNALGARNI <s.xooxs@hotmail.com>
2025-10-17 08:38:34 +0000

    2-add_node.c

--- singly_linked_lists/main.c
+++ singly_linked_lists/main.c
@@ -2,40 +2,42 @@
 #include <stdlib.h>
 #include <string.h>
 #include "lists.h"
+#include "lists.h"
+
+int _putchar(char c);
+
+void print_string(char *str)
+{
+    int i = 0;
+
+    while (str[i] != '\0')
+    {
+        _putchar(str[i]);
+        i++;
+    }
+}
+
+void print_list(list_t *head)
+{
+    list_t *temp = head;
+
+    while (temp != NULL)
+    {
+        print_string(temp->str);
+        _putchar('\n');
+        temp = temp->next;
+    }
+}
 
 int main(void)
 {
-    list_t *node1 = malloc(sizeof(list_t));
-    list_t *node2 = malloc(sizeof(list_t));
-    list_t *node3 = malloc(sizeof(list_t));
-    list_t *head;
+    list_t *head = NULL;
 
-    if (!node1 || !node2 || !node3)
-        return (1);
+    add_node(&head, "Hello");
+    add_node(&head, "World");
+    add_node(&head, "ChatGPT");
 
-    node1->str = strdup("One");
-    node1->len = 3;
-    node1->next = node2;
-
-    node2->str = strdup("Two");
-    node2->len = 3;
-    node2->next = node3;
-
-    node3->str = strdup("Three");
-    node3->len = 5;
-    node3->next = NULL;
-
-    head = node1;
-
-    printf("Number of elements: %lu\n", (unsigned long) list_len(head));
-
-    /* Free memory */
-    free(node1->str);
-    free(node1);
-    free(node2->str);
-    free(node2);
-    free(node3->str);
-    free(node3);
+    print_list(head);
 
     return (0);
 }
