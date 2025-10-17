ead800a0c46a7e50099de5e29fc8045515d60221
OWNALGARNI <s.xooxs@hotmail.com>
2025-10-17 12:49:06 +0000

    4-free_list.c

--- singly_linked_lists/main.c
+++ singly_linked_lists/main.c
@@ -1,43 +1,21 @@
 #include <stdio.h>
 #include "lists.h"
 
-/* Function prototype to print the list */
-size_t print_list(const list_t *h);
-
+/**
+ * main - test add_node and free_list functions
+ *
+ * Return: 0 on success
+ */
 int main(void)
 {
     list_t *head = NULL;
-    list_t *node;
 
-    node = add_node_end(&head, "Hello");
-    if (node == NULL)
-        return (1);
+    add_node(&head, "Hello");
+    add_node(&head, "World");
+    add_node(&head, "Linked List");
 
-    node = add_node_end(&head, "World");
-    if (node == NULL)
-        return (1);
 
-    node = add_node_end(&head, "!");
-    if (node == NULL)
-        return (1);
-
-    print_list(head);
-
-    /* Here you should ideally free the list */
+    free_list(head);
 
     return (0);
 }
-
-/* Simple print_list implementation for testing */
-size_t print_list(const list_t *h)
-{
-    size_t count = 0;
-
-    while (h)
-    {
-        printf("[%u] %s\n", h->len, h->str);
-        h = h->next;
-        count++;
-    }
-    re
-
