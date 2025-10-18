800777de4491b1e10d5a301629e1a39944cccfa2
OWNALGARNI <s.xooxs@hotmail.com>
2025-10-18 01:25:43 +0000

    0-print_list.c

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,21 +1,15 @@
-# include <stdio.h>
-# include "lists.h"
+#include <stdio.h>
+#include "lists.h"
 
 /**
-* print_list print all elements of a list _t list.
-*
-* This function traverses a singly linked list and prints
-* the string and length of each node in the format: [len] str.
-* if a node has NULL string, it prints [0] (nil).
-*
-* @h: pointer to the first node in the list
-*
-* Return: The  number of nodes in the list
-*/
-
+ * print_list - Prints all the elements of a list_t list
+ * @h: Pointer to the head of the list
+ * Return: The number of nodes
+ */
 size_t print_list(const list_t *h)
 {
 size_t count = 0;
+
 while (h != NULL)
 {
 if (h->str == NULL)
