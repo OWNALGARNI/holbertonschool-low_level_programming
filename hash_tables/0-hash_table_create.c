1813641d6528553388d7e9c4237f2f41c2c8891f
OWNALGARNI <s.xooxs@hotmail.com>
2025-10-22 09:01:32 +0000

    0-hash_table_create.c

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Creates a hash table of a given size
+ * @size: Number of slots in the hash table
+ * Return: Pointer to the new hash table, or NULL on failure
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+    hash_table_t *table;
+    unsigned long int i;
+
+    table = malloc(sizeof(hash_table_t));
+    if (table == NULL)
+        return NULL;
+
+    table->size = size;
+
+    table->array = malloc(sizeof(hash_node_t *) * size);
+    if (table->array == NULL)
+    {
+        free(table);
+        return NULL;
+    }
+
+    for (i = 0; i < size; i++)
+        table->array[i] = NULL;
+
+    return table;
+}
