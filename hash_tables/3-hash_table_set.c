7c6c5ccff9228c4d68a1fd6358a204a3340a0e4b
OWNALGARNI <s.xooxs@hotmail.com>
2025-10-23 08:50:08 +0000

    4-hash_table_get.c

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -16,57 +16,49 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-    hash_node_t *node, *new_node;
-    char *value_copy;
-    unsigned long int index;
+hash_node_t *node, *new_node;
+char *value_copy;
+unsigned long int index;
 
-  
-    if (ht == NULL || key == NULL || key[0] == '\0')
-        return (0);
+if (ht == NULL || key == NULL || key[0] == '\0')
+return (0);
 
-  
-    value_copy = strdup(value);
-    if (value_copy == NULL)
-        return (0);
+value_copy = strdup(value);
+if (value_copy == NULL)
+return (0);
 
-   
-    index = key_index((const unsigned char *)key, ht->size);
+index = key_index((const unsigned char *)key, ht->size);
 
-  
-    node = ht->array[index];
-    while (node)
-    {
-        if (strcmp(node->key, key) == 0)
-        {
-      
-            free(node->value);
-            node->value = value_copy;
-            return (1);
-        }
-        node = node->next;
-    }
+node = ht->array[index];
+while (node)
+{
+if (strcmp(node->key, key) == 0)
+{
+free(node->value);
+node->value = value_copy;
+return (1);
+}
+node = node->next;
+}
 
-  
-    new_node = malloc(sizeof(hash_node_t));
-    if (new_node == NULL)
-    {
-        free(value_copy);
-        return (0);
-    }
+new_node = malloc(sizeof(hash_node_t));
+if (new_node == NULL)
+{
+free(value_copy);
+return (0);
+}
 
-   
-    new_node->key = strdup(key);
-    if (new_node->key == NULL)
-    {
-        free(value_copy);
-        free(new_node);
-        return (0);
-    }
+new_node->key = strdup(key);
+if (new_node->key == NULL)
+{
+free(value_copy);
+free(new_node);
+return (0);
+}
 
-  
-    new_node->value = value_copy;
-    new_node->next = ht->array[index];
-    ht->array[index] = new_node;
+new_node->value = value_copy;
+new_node->next = ht->array[index];
+ht->array[index] = new_node;
 
-    return (1);
+return (1);
 }
